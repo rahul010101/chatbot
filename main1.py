4b1192c957adbfbee1446d770bdfbca4ce6955c2
Rahul_nai <56308515+rahul010101@users.noreply.github.com>
2024-04-29 13:38:32 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -3,40 +3,57 @@ from telegram import Update, InputFile
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 import pdfkit
 from urllib.parse import urlparse, parse_qs
+import pyjokes
+import requests
 
-TOKEN: Final = 'YOUR_BOT_TOKEN'
+TOKEN: Final = 'YOUR_TELEGRAM_TOKEN'
 BOT_USERNAME: Final = '@rahulnai_bot'
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Hello! Thanks for chatting with me! I am a new chatbot!')
+    user_name = update.message.from_user.first_name
+    reply_message = f'''Hello, {user_name}\nWelcome to the chatbot!'''
+    await update.message.reply_text(reply_message)
+
+'''def generate_joke() -> str:
+    joke = pyjokes.get_joke()
+    return f"Here's a joke for you:\n{joke}"'''
+
+async def joke_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    url = "https://icanhazdadjoke.com/slack"
+    response = requests.get(url)
+    joke_data = response.json()
+    joke_text = joke_data["attachments"][0]["text"]
+    await update.message.reply_text(joke_text)
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('''You can type /start or /help or /link''')
+    await update.message.reply_text('''You can type:\n/start\n/help\n/link\n/joke\nAnd for normal text you can type:\nhi\nhow are you\nwhat can you do\nhow old are you\nthank you or thanks''')
 
-def handle_response(text: str) -> str:
+def handle_response(text: str, user_name: str) -> str:
     processed: str = text.lower()
-    if 'hello' in processed:
-        return 'Hey there!'
+    if 'hi' in processed:
+        return f"Hi, {user_name}!\n"
     if 'how are you' in processed:
-        return 'I am good!'
-    if 'i love python' in processed:
-        return 'Remember to subscribe!'
-    return 'I do not understand what you wrote...'
+        return f"I am doing great, {user_name}! Thank you for asking!"
+    if 'what can you do' in processed:
+        return f"I can help you with tasks such as converting an webpage in to an pdf and give you a pdf file, {user_name}. You want to try type ' /link https://yourlink.com '"
+    if 'how old are you' in processed:
+        return f"I am a chatbot, so I don't have an age, {user_name}. But I am always here to assist you!"
+    if 'thank you' in processed or 'thanks' in processed:
+        return f"Anytime, {user_name}! I'm always here to help!"
+    return f"I'm sorry, {user_name}, but I do not understand what you wrote..."
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message_type: str = update.message.chat.type
     text: str = update.message.text
+    user_name: str = update.message.from_user.first_name
+    
     print(f'User ({update.message.chat.id}) in {message_type}: "{text}"')
-    if message_type == 'group':
-        if BOT_USERNAME in text:
-            new_text: str = text.replace(BOT_USERNAME, '').strip()
-            response: str = handle_response(new_text)
-        else:
-            return
-    else:
-        response: str = handle_response(text)
+    
+    response = handle_response(text, user_name)
+    
     print('Bot:', response)
     await update.message.reply_text(response)
+
 
 async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(f'Update {update} caused error {context.error}')
@@ -70,6 +87,7 @@ if __name__ == '__main__':
     app.add_handler(CommandHandler('start', start_command))
     app.add_handler(CommandHandler('help', help_command))
     app.add_handler(CommandHandler('link', link_command))
+    app.add_handler(CommandHandler('joke', joke_command))
     # Messages
     app.add_handler(MessageHandler(filters.TEXT, handle_message))
     # Errors
